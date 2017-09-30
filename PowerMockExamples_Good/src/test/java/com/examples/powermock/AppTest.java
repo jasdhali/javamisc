411ffe108fa890a59f873be6a3572f04b7fa2f40
Jaspal Dhaliwal <jasdhali@gmail.com>
2017-09-29 23:59:34 -0500

    Changes for _good

---
+++ PowerMockExamples_Good/src/test/java/com/examples/powermock/AppTest.java
@@ -0,0 +1,20 @@
+package com.examples.powermock;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
